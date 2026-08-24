45f4e3967e5aaa3ea6f3c16d3366be7fc8874e76
Kinetic Napier <u61819269@gmail.com>
2026-08-24 12:23:23 +0900

    Update runtime version to 0.1.9

--- src/Main.cs
+++ src/Main.cs
@@ -4,7 +4,7 @@ namespace KineticNapier.ADOFAIWorkbench
 {
     public static class Main
     {
-        internal const string Version = "0.1.8";
+        internal const string Version = "0.1.9";
         private static bool enabled;
 
         public static bool Load(UnityModManager.ModEntry entry)
