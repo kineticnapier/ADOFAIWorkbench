67f5107ea77e65b10a933563db848ca141a0b9ad
Kinetic Napier <u61819269@gmail.com>
2026-08-24 12:56:08 +0900

    Update Workbench assembly version to 0.3.0

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ADOFAIWorkbench")]
-[assembly: AssemblyDescription("Dockable IDE-style workspace shell for the ADOFAI level editor")]
+[assembly: AssemblyDescription("Standalone dockable WPF tool window for ADOFAI")]
 [assembly: AssemblyCompany("Kinetic Napier")]
 [assembly: AssemblyProduct("ADOFAIWorkbench")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9d75d4a1-5c0d-4ec2-8f3b-665b9c7ea5b2")]
-[assembly: AssemblyVersion("0.2.4.0")]
-[assembly: AssemblyFileVersion("0.2.4.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
