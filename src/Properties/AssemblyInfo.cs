a4b4539a4ea155ae178f53e4614e3db60590f10b
Kinetic Napier <u61819269@gmail.com>
2026-08-24 13:52:18 +0900

    Set Workbench assembly version 0.6.0

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ADOFAIWorkbench")]
-[assembly: AssemblyDescription("Standalone DockPanel Suite workspace window for ADOFAI")]
+[assembly: AssemblyDescription("ADOFAI bridge for an external DockPanel Suite workspace host")]
 [assembly: AssemblyCompany("Kinetic Napier")]
 [assembly: AssemblyProduct("ADOFAIWorkbench")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9d75d4a1-5c0d-4ec2-8f3b-665b9c7ea5b2")]
-[assembly: AssemblyVersion("0.5.2.0")]
-[assembly: AssemblyFileVersion("0.5.2.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
