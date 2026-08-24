0e62be93df58e42c61217fa4cc36266322bf0693
Kinetic Napier <u61819269@gmail.com>
2026-08-24 12:07:44 +0900

    Update runtime version to 0.1.7

--- src/Main.cs
+++ src/Main.cs
@@ -4,7 +4,7 @@ namespace KineticNapier.ADOFAIWorkbench
 {
     public static class Main
     {
-        internal const string Version = "0.1.6";
+        internal const string Version = "0.1.7";
         private static bool enabled;
 
         public static bool Load(UnityModManager.ModEntry entry)
