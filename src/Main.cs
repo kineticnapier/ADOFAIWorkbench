38465fc0e812d6deed3a96ba527e53a37349a84f
Kinetic Napier <u61819269@gmail.com>
2026-08-24 13:27:55 +0900

    Bump Workbench to 0.5.0

--- src/Main.cs
+++ src/Main.cs
@@ -4,7 +4,7 @@ namespace KineticNapier.ADOFAIWorkbench
 {
     public static class Main
     {
-        internal const string Version = "0.4.1";
+        internal const string Version = "0.5.0";
         private static bool enabled;
 
         public static bool Load(UnityModManager.ModEntry entry)
@@ -12,7 +12,7 @@ namespace KineticNapier.ADOFAIWorkbench
             entry.OnToggle = OnToggle;
             entry.OnUpdate = OnUpdate;
             Workbench.RegisterPaneProvider(new WelcomePaneProvider());
-            entry.Logger.Log("ADOFAI Workbench v" + Version + " loaded (standalone recursive docking window).");
+            entry.Logger.Log("ADOFAI Workbench v" + Version + " loaded (DockPanel Suite standalone window).");
             return true;
         }
 
