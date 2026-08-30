7238218087cfb7b891bdb3c7d116c76dbe9d2c5e
Kinetic Napier <u61819269@gmail.com>
2026-08-30 16:09:30 +0900

    Bump Workbench to 0.8.5

--- src/Main.cs
+++ src/Main.cs
@@ -7,7 +7,7 @@ namespace KineticNapier.ADOFAIWorkbench
 {
     public static class Main
     {
-        internal const string Version = "0.8.4";
+        internal const string Version = "0.8.5";
         private static bool enabled;
         private static UnityModManager.ModEntry modEntry;
 
